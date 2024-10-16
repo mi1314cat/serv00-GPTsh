42826f9756e5a4e3ac74870a62696fc455eaadb9
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-16 20:35:03 +0800

    Update crtest.sh

--- crtest.sh
+++ crtest.sh
@@ -16,7 +16,7 @@ CRON_PM2_JOB="*/12 * * * * $PM2_PATH resurrect >> ${USER_HOME}/pm2_resurrect.log
 # 定义函数来添加 crontab 任务，减少重复代码
 add_cron_job() {
   local job=$1
-  (crontab -l 2>/dev/null | grep -F "$job") || (crontab -l 2>/dev/null; echo "$job") | crontab -
+  (crontab -l 2>/dev/null | grep -F "$job" > /dev/null) || (echo "$job"; crontab -l 2>/dev/null) | crontab -
 }
 
 # 检查并添加 crontab 任务
@@ -34,5 +34,9 @@ else
     add_cron_job "$CRON_HYSTERIA_CHECK"
   fi
 fi
+
+# 输出当前 crontab 任务列表，便于检查
+echo "当前 crontab 任务列表:"
+crontab -l
 
 echo "crontab 任务添加完成"
