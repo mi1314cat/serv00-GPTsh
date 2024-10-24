85272f6244cff76b46a855b7e972e2eb4a3a59f2
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-24 17:51:34 +0800

    Update crtest.sh

--- crtest.sh
+++ crtest.sh
@@ -4,12 +4,6 @@
 USER=$(whoami)
 USER_LOWER="${USER,,}"
 USER_HOME="/home/${USER_LOWER}"
-HYSTERIA_WORKDIR="${USER_HOME}/.hysteria"
-HYSTERIA_CONFIG="${HYSTERIA_WORKDIR}/config.yaml"  # Hysteria 配置文件路径
-
-# 定义 crontab 任务
-CRON_HYSTERIA_START="@reboot pkill -kill -u $USER && nohup ${HYSTERIA_WORKDIR}/web server -c ${HYSTERIA_CONFIG} >/dev/null 2>&1 &"
-CRON_HYSTERIA_CHECK="*/12 * * * * pgrep -x \"web\" > /dev/null || nohup ${HYSTERIA_WORKDIR}/web server -c ${HYSTERIA_CONFIG} >/dev/null 2>&1 &"
 PM2_PATH="${USER_HOME}/.npm-global/lib/node_modules/pm2/bin/pm2"
 CRON_PM2_JOB="*/12 * * * * $PM2_PATH resurrect >> ${USER_HOME}/pm2_resurrect.log 2>&1"
 
@@ -33,13 +27,6 @@ echo "检查并添加 crontab 任务"
 if command -v pm2 > /dev/null 2>&1 && [[ $(which pm2) == "${USER_HOME}/.npm-global/bin/pm2" ]]; then
   echo "已安装 pm2 ，启用 pm2 保活任务"
   add_cron_job "$CRON_PM2_JOB"
-else
-  # Hysteria 的重启任务
-  if [ -f "$HYSTERIA_CONFIG" ]; then
-    echo "添加 Hysteria 的 crontab 重启任务"
-    add_cron_job "$CRON_HYSTERIA_START"
-    add_cron_job "$CRON_HYSTERIA_CHECK"
-  fi
 fi
 
 # 输出当前 crontab 任务列表，便于检查
