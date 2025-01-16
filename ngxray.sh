9b81775e1aff7d8284b6feb36e371a394f951bab
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-01-16 19:33:41 +0800

    Update ngxray.sh

--- ngxray.sh
+++ ngxray.sh
@@ -259,7 +259,19 @@ cat <<EOF > $USER_HOME/catmi/xray/config.json
     ]
 }
 EOF
+# 定义 crontab 任务
+CRON_JOB="0 */12 * * * screen -S xray $USER_HOME/catmi/xray/xray run"
 
+# 定义函数来添加 crontab 任务，减少重复代码
+add_cron_job() {
+  local job=$1
+  (crontab -l 2>/dev/null | grep -F "$job") || (crontab -l 2>/dev/null; echo "$job") | crontab -
+}
+
+# 添加 crontab 任务
+echo "检查并添加 crontab 任务"
+add_cron_job "$CRON_JOB"
+echo "crontab 任务添加完成"
 # 启动 Xray
 screen -S xray $USER_HOME/catmi/xray/xray run
 }
