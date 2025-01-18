8c764a6631680c3616c5520ed8ec08b420090750
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-01-18 21:19:14 +0800

    Update crtest.sh

--- crtest.sh
+++ crtest.sh
@@ -1,14 +1,14 @@
-
-
 #!/bin/bash
 
 # 获取当前用户名
 USER=$(whoami)
 USER_LOWER="${USER,,}"
 USER_HOME="/home/${USER_LOWER}"
+HYSTERIA_WORKDIR="${USER_HOME}/catmi/hy2"
 
 # 定义 crontab 任务
-CRON_JOB="0 */12 * * * screen -S xray ${USER_HOME}/catmi/xray/xray run"
+XRAY_CRON_JOB="0 */12 * * * screen -S xray ${USER_HOME}/catmi/xray/xray run"
+HY2_CRON_JOB="0 */12 * * * screen -dmS hy2 $HYSTERIA_WORKDIR/web server -c $HYSTERIA_WORKDIR/config.yaml"
 
 # 定义函数来添加 crontab 任务，减少重复代码
 add_cron_job() {
@@ -17,6 +17,10 @@ add_cron_job() {
 }
 
 # 添加 crontab 任务
-echo "检查并添加 crontab 任务"
-add_cron_job "$CRON_JOB"
-echo "crontab 任务添加完成"
+echo "检查并添加 xray 的 crontab 任务"
+add_cron_job "$XRAY_CRON_JOB"
+
+echo "检查并添加 hy2 的 crontab 任务"
+add_cron_job "$HY2_CRON_JOB"
+
+echo "所有 crontab 任务添加完成"
