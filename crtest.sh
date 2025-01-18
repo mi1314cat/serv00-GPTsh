11ea7229d9a198a43fff4eec9fe8ad9ac4afdaae
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-01-18 21:41:34 +0800

    Update crtest.sh

--- crtest.sh
+++ crtest.sh
@@ -8,7 +8,7 @@ HYSTERIA_WORKDIR="${USER_HOME}/catmi/hy2"
 
 # 定义 crontab 任务
 XRAY_CRON_JOB="0 */12 * * * screen -S xray ${USER_HOME}/catmi/xray/xray run"
-HY2_CRON_JOB="0 */12 * * * screen -dmS hy2 $HYSTERIA_WORKDIR/web server -c $HYSTERIA_WORKDIR/config.yaml"
+HY2_CRON_JOB="0 */12 * * * screen -S hy2 $HYSTERIA_WORKDIR/web server -c $HYSTERIA_WORKDIR/config.yaml"
 
 # 定义函数来添加 crontab 任务，减少重复代码
 add_cron_job() {
